163be7ada2f11b77c43e5afc7cb9016fefc56f65
arendgeerlofs <arendgeerlofs@hotmail.nl>
2023-10-31 15:19:23 +0100

    Split functions and added mc_integration
    Split mandelbrot and complex system functions into a seperate python file, furthermore added 1st version of mc integration

---
+++ mandelbrot.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
+    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
+    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
+
+def mandelbrot_matrix(matrix, iterations):
+    for i in range(np.shape(matrix)[0]):
+        for j in range(np.shape(matrix)[1]):
+            value = mandelbrot(matrix[i][j], iterations)
+            matrix[i][j] = value
+    return matrix
+
+def mandelbrot(c, iterations):
+    z = 0
+    for _ in range(iterations):
+        z = z**2 + c
+    return z >= -2 and z <= 2
