12fabbe03d420cbbf6b754b3c8516d0d9a61efb6
arendgeerlofs <arendgeerlofs@hotmail.nl>
2023-11-06 15:53:17 +0100

    Latin hypercube fix and quick stop mandelbrot
    Fixed functionality for latin hypercube sampling by changing how we choose random point and amount of samples. Furthermore added stop condition in mandelbrot function

--- functions.py
+++ functions.py
@@ -17,9 +17,11 @@ def mandelbrot(c, iterations):
     z = 0
     for _ in range(iterations):
         z = z**2 + c
+        if abs(z) > 2:
+            return False
     return abs(z) <= 2
 
-def MC_integration(iterations, samples, randomS=False, LatinS=False):
+def MC_integration(iterations, samples, randomS=False, LatinS=False, Orthog=False):
     
     xmin = -2
     xmax = 2
@@ -27,6 +29,7 @@ def MC_integration(iterations, samples, randomS=False, LatinS=False):
     ymax = 2
     total_area = abs(xmax - xmin)*abs(ymax-ymin)
     
+    # TODO choose pixel density/add to function variables
     matrix = complex_matrix(xmin, xmax, ymin, ymax, 100)
     mb_matrix = mandelbrot(matrix, iterations)
     
@@ -38,6 +41,9 @@ def MC_integration(iterations, samples, randomS=False, LatinS=False):
     if LatinS:
         counter, samples = Latin_hypercube(mb_matrix, samples)
 
+    if Orthog:
+        counter, samples = Orthogonal_sampling(mb_matrix, samples)
+    
     return total_area*counter/samples
 
 def random_sampling(mb_matrix, samples):
@@ -54,23 +60,45 @@ def random_sampling(mb_matrix, samples):
 
 def Latin_hypercube(mb_matrix, samples):
     counter = 0
-    rows = []
-    columns = []
     
-    if samples > np.shape(mb_matrix)[0]*np.shape(mb_matrix)[1]:
-        samples = np.shape(mb_matrix)[0]*np.shape(mb_matrix)[1]
+    if samples > max(np.shape(mb_matrix)[0], np.shape(mb_matrix)[1]):
+        samples = max(np.shape(mb_matrix)[0], np.shape(mb_matrix)[1])
         print("The amount of samples used, was corrected to", samples)
-        
-    while len(rows) < np.shape(mb_matrix)[0]:
-        while len(columns) < np.shape(mb_matrix)[1]:
-            i = np.random.randint(0, np.shape(mb_matrix)[0]-1)
-            j = np.random.randint(0, np.shape(mb_matrix)[1]-1)
-            
-            if i not in rows and j not in columns:
-                rows.append(i)
-                columns.append(j)
-                
-                if np.real(mb_matrix[i,j]) == 1:
-                    counter += 1
+    
+
+    row_indexes = [v for v in range(np.shape(mb_matrix)[0])]
+    column_indexes = [v for v in range(np.shape(mb_matrix)[1])]
+    for i in range(samples):
+        i = np.random.choice(row_indexes)
+        j = np.random.choice(column_indexes)
+        if np.real(mb_matrix[i,j]) == 1:
+                counter += 1
+        row_indexes.remove(i)
+        column_indexes.remove(j)
+                    
+    return counter, samples
+
+def Orthogonal_sampling(mb_matrix, samples):
+    counter = 0
+    
+    if samples > max(np.shape(mb_matrix)[0], np.shape(mb_matrix)[1]):
+        samples = max(np.shape(mb_matrix)[0], np.shape(mb_matrix)[1])
+        print("The amount of samples used, was corrected to", samples)
+
+    row_indexes = [v for v in range(np.shape(mb_matrix)[0])]
+    column_indexes = [v for v in range(np.shape(mb_matrix)[1])]
+    square1 = [[v for v in range(np.shape(mb_matrix)[0])], [v for v in range(np.shape(mb_matrix)[1])]]
+    square2 = [[v for v in range(np.shape(mb_matrix)[0])], [v for v in range(np.shape(mb_matrix)[1])]]
+    square3 = [[v for v in range(np.shape(mb_matrix)[0])], [v for v in range(np.shape(mb_matrix)[1])]]
+    square4 = [[v for v in range(np.shape(mb_matrix)[0])], [v for v in range(np.shape(mb_matrix)[1])]]
+
+
+    for i in range(samples):
+        i = np.random.choice(row_indexes)
+        j = np.random.choice(column_indexes)
+        if np.real(mb_matrix[i,j]) == 1:
+                counter += 1
+        row_indexes.remove(i)
+        column_indexes.remove(j)
                     
     return counter, samples
